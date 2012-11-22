f8806abac3432d527dfaae91e089c1d294dd6bd3
Maurits van der Schee <m.vanderschee@leaseweb.com>
2012-11-22 23:22:53 +0100

    Added new HTTP calls: Delete, Post and Put

---
+++ Call/HttpDeleteJson.php
@@ -0,0 +1,29 @@
+<?php
+namespace Lsw\ApiCallerBundle\Call;
+
+/**
+ * cURL based API call with request data send as GET parameters
+ *
+ * @author Maurits van der Schee <m.vanderschee@leaseweb.com>
+ */
+class HttpDeleteJson extends ApiCall implements ApiCallInterface
+{
+  public function generateRequestData()
+  {
+    $this->requestData = http_build_query($this->requestObject);
+  }
+  
+  public function parseResponseData()
+  {
+    $this->responseObject = json_decode($this->responseData);
+  }
+  
+  public function makeRequest($curlHandle, $options)
+  {
+    curl_setopt($curlHandle, CURLOPT_URL, $this->url.'?'.$this->requestData);
+    curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST, 'DELETE');
+    curl_setopt_array($curlHandle, $options);
+    $this->responseData = curl_exec($curlHandle);
+  }
+  
+}
