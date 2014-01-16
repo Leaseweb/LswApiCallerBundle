9f7b01438d8b92a16dbb2f19ad0c75c782893cd5
Dmitry Parnas <parnas@parnas.org>
2014-01-16 13:10:01 +0100

    If curl have returned false responseHeader and responseBody both would be empty (previously error was thrown)

--- Call/CurlCall.php
+++ Call/CurlCall.php
@@ -266,12 +266,12 @@ abstract class CurlCall implements ApiCallInterface
      */
     protected function assignResponseValues()
     {
+        $headers = $body = '';
+
         if($this->responseRaw) {
             $parts = explode("\r\n\r\nHTTP/", $this->responseRaw);
             $parts = (count($parts) > 1 ? 'HTTP/' : '').array_pop($parts);
             list($headers, $body) = explode("\r\n\r\n", $parts, 2);
-        } else {
-            $headers = $body = '';
         }
 
         $this->responseHeaders = $headers;
