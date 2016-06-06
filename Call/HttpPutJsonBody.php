dfde4aeb13fcc8b680976e2d7c1e1d3f63e54f85
Maurits van der Schee <maurits@vdschee.nl>
2016-06-06 17:05:15 +0200

    Create HttpPutJsonBody.php

---
+++ Call/HttpPutJsonBody.php
@@ -0,0 +1,37 @@
+<?php
+namespace Lsw\ApiCallerBundle\Call;
+use Lsw\ApiCallerBundle\Helper\Curl;
+/**
+ * cURL based API call with request data send as JSON body
+ *
+ * @author Maurits van der Schee <m.vanderschee@leaseweb.com>
+ */
+class HttpPutJsonBody extends CurlCall implements ApiCallInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function generateRequestData()
+    {
+        $this->requestData = json_encode($this->requestObject);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function parseResponseData()
+    {
+        $this->responseObject = json_decode($this->responseData,$this->asAssociativeArray);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function makeRequest($curl, $options)
+    {
+        $curl->setopt(CURLOPT_URL, $this->url);
+        $curl->setopt(CURLOPT_POST, 1);
+        $curl->setopt(CURLOPT_POSTFIELDS, $this->requestData);
+        $curl->setopt(CURLOPT_CUSTOMREQUEST, "PUT");
+        $curl->setoptArray($options);
+        $this->curlExec($curl);
+    }
+}
