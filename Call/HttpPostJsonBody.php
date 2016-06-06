047f7438f1b1cecb8f7f704e0c7966b0a6e98998
Maurits van der Schee <maurits@vdschee.nl>
2016-06-06 16:58:10 +0200

    Create HttpPostJsonBody.php

---
+++ Call/HttpPostJsonBody.php
@@ -0,0 +1,36 @@
+<?php
+namespace Lsw\ApiCallerBundle\Call;
+/**
+ * cURL based API call with request data send as POST parameters
+ * Response is parsed as JSON
+ *
+ * @author Maurits van der Schee <m.vanderschee@leaseweb.com>
+ */
+class HttpPostJsonBody extends HttpPost implements ApiCallInterface
+{
+    /**
+    * {@inheritdoc}
+    */
+    public function generateRequestData()
+    {
+        $this->requestData = json_encode($this->requestObject);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function parseResponseData()
+    {
+        $this->responseObject = json_decode($this->responseData,$this->asAssociativeArray);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function makeRequest($curl, $options)
+    {
+        $curl->setopt(CURLOPT_URL, $this->url);
+        $curl->setopt(CURLOPT_POST, 1);
+        $curl->setopt(CURLOPT_POSTFIELDS, $this->requestData);
+        $curl->setoptArray($options);
+        $this->curlExec($curl);
+    }
+}
